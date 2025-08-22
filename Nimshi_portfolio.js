244fe4ecb7ee7dba75bbc60d9c437a37867e6429
Nimshi <nimshicp2003@gmail.com>
2025-08-22 20:29:35 +0530

    complete design of portfolio

---
+++ Nimshi_portfolio.js
@@ -0,0 +1,24 @@
+function sendMail(){
+    var params = {
+        name: document.getElementById("name").value ,
+        email: document.getElementById("email").value ,
+        message: document.getElementById("message").value
+
+    };
+
+
+const serviceID = "service_32xapco";
+const templateID = "template_nbwcbk4"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sented successfully")
+    }
+)
+.catch((err) => console.log(err));
+}
